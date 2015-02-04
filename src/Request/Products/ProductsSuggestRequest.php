0c0622f9161aca7761e741477cded70eca15a29f
Jens Schulze <jens.schulze@commercetools.de>
2015-02-04 15:47:02 +0100

    add search keywords to the suggest constructor

--- src/Request/Products/ProductsSuggestRequest.php
+++ src/Request/Products/ProductsSuggestRequest.php
@@ -6,22 +6,30 @@
 
 namespace Sphere\Core\Request\Products;
 
+use Sphere\Core\Model\Type\LocalizedString;
 use Sphere\Core\Request\AbstractProjectionRequest;
 use Sphere\Core\Request\Endpoints\ProductProjectionsEndpoint;
+use Sphere\Core\Response\SingleResourceResponse;
 
 /**
  * Class ProductsSearchRequest
  * @package Sphere\Core\Request\Products
- * @method static ProductsSuggestRequest of()
+ * @method static ProductsSuggestRequest of(LocalizedString $keywords)
  */
 class ProductsSuggestRequest extends AbstractProjectionRequest
 {
     /**
-     *
+     * @var LocalizedString
      */
-    public function __construct()
+    protected $searchKeywords;
+
+    /**
+     * @param LocalizedString $keywords
+     */
+    public function __construct(LocalizedString $keywords)
     {
         parent::__construct(ProductProjectionsEndpoint::endpoint());
+        $this->addKeywords($keywords);
     }
 
     /**
@@ -30,5 +38,46 @@ class ProductsSuggestRequest extends AbstractProjectionRequest
     protected function getProjectionAction()
     {
         return 'suggest';
+    }
+
+    /**
+     * @param LocalizedString $localizedString
+     */
+    public function addKeywords(LocalizedString $localizedString)
+    {
+        $this->getSearchKeywords()->merge($localizedString);
+    }
+
+    /**
+     * @param string $locale
+     * @param string $keyword
+     */
+    public function addKeyword($locale, $keyword)
+    {
+        $this->getSearchKeywords()->add($locale, $keyword);
+    }
+
+    /**
+     * @return LocalizedString
+     */
+    public function getSearchKeywords()
+    {
+        if (is_null($this->searchKeywords)) {
+            $this->searchKeywords = new LocalizedString([]);
+        }
+        return $this->searchKeywords;
+    }
+
+    /**
+     * @param LocalizedString $searchKeywords
+     */
+    public function setSearchKeywords(LocalizedString $searchKeywords)
+    {
+        $this->searchKeywords = $searchKeywords;
+    }
+
+    public function buildResponse($response)
+    {
+        return new SingleResourceResponse($response, $this);
     }
 }
