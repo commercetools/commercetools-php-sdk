c2daa62e4f0d1e5eb2154c95cb8cf03b1ffa7590
Jens Schulze <jens.schulze@commercetools.de>
2015-02-20 15:11:06 +0100

    change inheritance of slug and sku query

--- src/Request/Products/ProductProjectionFetchBySkuRequest.php
+++ src/Request/Products/ProductProjectionFetchBySkuRequest.php
@@ -5,50 +5,20 @@
 
 namespace Sphere\Core\Request\Products;
 
-
-use Sphere\Core\Client\HttpMethod;
-use Sphere\Core\Client\HttpRequest;
-use Sphere\Core\Request\AbstractApiRequest;
-use Sphere\Core\Request\QueryTrait;
-use Sphere\Core\Request\StagedTrait;
-use Sphere\Core\Response\PagedQueryResponse;
-use Sphere\Core\Response\SingleResourceResponse;
-
-class ProductProjectionFetchBySkuRequest extends AbstractApiRequest
+/**
+ * Class ProductProjectionFetchBySkuRequest
+ * @package Sphere\Core\Request\Products
+ */
+class ProductProjectionFetchBySkuRequest extends ProductProjectionQueryRequest
 {
-    use QueryTrait;
-    use StagedTrait;
-
-    const SKU = 'sku';
-
     /**
      * @param string $sku
      */
     public function __construct($sku)
     {
-        parent::__construct(ProductSearchEndpoint::endpoint());
+        parent::__construct();
         if (!is_null($sku)) {
             $this->where(sprintf('masterVariant(sku="%1$s") or variants(sku="%1$s")', $sku));
         }
-    }
-
-    /**
-     * @return HttpRequest
-     * @internal
-     */
-    public function httpRequest()
-    {
-        $request = new HttpRequest(HttpMethod::GET, $this->getPath());
-        return $request;
-    }
-
-    /**
-     * @param $response
-     * @return PagedQueryResponse
-     * @internal
-     */
-    public function buildResponse($response)
-    {
-        return new PagedQueryResponse($response, $this);
     }
 }
--- src/Request/Products/ProductProjectionFetchBySlugRequest.php
+++ src/Request/Products/ProductProjectionFetchBySlugRequest.php
@@ -6,28 +6,21 @@
 namespace Sphere\Core\Request\Products;
 
 
-use Sphere\Core\Client\HttpMethod;
-use Sphere\Core\Client\HttpRequest;
 use Sphere\Core\Model\Common\Context;
-use Sphere\Core\Request\AbstractApiRequest;
-use Sphere\Core\Request\QueryTrait;
-use Sphere\Core\Request\StagedTrait;
-use Sphere\Core\Response\PagedQueryResponse;
 
-class ProductProjectionFetchBySlugRequest extends AbstractApiRequest
+/**
+ * Class ProductProjectionFetchBySlugRequest
+ * @package Sphere\Core\Request\Products
+ */
+class ProductProjectionFetchBySlugRequest extends ProductProjectionQueryRequest
 {
-    use QueryTrait;
-    use StagedTrait;
-
-    const SKU = 'sku';
-
     /**
      * @param string $slug
      * @param Context $context
      */
     public function __construct($slug, Context $context)
     {
-        parent::__construct(ProductSearchEndpoint::endpoint());
+        parent::__construct();
         $parts = array_map(
             function ($value) {
                 return sprintf('slug(%s="%s")', $value, '%1$s');
@@ -37,25 +30,5 @@ class ProductProjectionFetchBySlugRequest extends AbstractApiRequest
         if (!is_null($slug) && !empty($parts)) {
             $this->where(sprintf(implode(' or ', $parts), $slug));
         }
-    }
-
-    /**
-     * @return HttpRequest
-     * @internal
-     */
-    public function httpRequest()
-    {
-        $request = new HttpRequest(HttpMethod::GET, $this->getPath());
-        return $request;
-    }
-
-    /**
-     * @param $response
-     * @return PagedQueryResponse
-     * @internal
-     */
-    public function buildResponse($response)
-    {
-        return new PagedQueryResponse($response, $this);
     }
 }
