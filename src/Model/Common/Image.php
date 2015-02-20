a639a36b05341719f8125fb18682fcb272ee2e66
Jens Schulze <jens.schulze@commercetools.de>
2015-02-20 17:08:11 +0100

    add getter for image urls

--- src/Model/Common/Image.php
+++ src/Model/Common/Image.php
@@ -17,6 +17,12 @@ namespace Sphere\Core\Model\Common;
  */
 class Image extends JsonObject
 {
+    const THUMB = 'thumb';
+    const SMALL = 'small';
+    const MEDIUM = 'medium';
+    const LARGE = 'large';
+    const ZOOM = 'zoom';
+
     public function getFields()
     {
         return [
@@ -24,5 +30,64 @@ class Image extends JsonObject
             'dimensions' => [static::TYPE => 'array'],
             'label' => [static::TYPE => 'string']
         ];
+    }
+
+    /**
+     * @param string $size
+     * @return string
+     */
+    protected function getThumbnailUrl($size = null)
+    {
+        if (empty($size)) {
+            return $this->getUrl();
+        }
+        $fileInfo = pathinfo($this->getUrl());
+
+        $dirName = isset($fileInfo['dirname']) ? $fileInfo['dirname'] : '';
+        $fileName = isset($fileInfo['filename']) ? $fileInfo['filename'] : '';
+        $extension = isset($fileInfo['extension']) ? '.' . $fileInfo['extension'] : '';
+        $url = $dirName . '/' . $fileName . '-' . $size . $extension;
+
+        return $url;
+    }
+
+    /**
+     * @return string
+     */
+    public function getThumb()
+    {
+        return $this->getThumbnailUrl(static::THUMB);
+    }
+
+    /**
+     * @return string
+     */
+    public function getSmall()
+    {
+        return $this->getThumbnailUrl(static::SMALL);
+    }
+
+    /**
+     * @return string
+     */
+    public function getMedium()
+    {
+        return $this->getThumbnailUrl(static::MEDIUM);
+    }
+
+    /**
+     * @return string
+     */
+    public function getLarge()
+    {
+        return $this->getThumbnailUrl(static::LARGE);
+    }
+
+    /**
+     * @return string
+     */
+    public function getZoom()
+    {
+        return $this->getThumbnailUrl(static::ZOOM);
     }
 }
