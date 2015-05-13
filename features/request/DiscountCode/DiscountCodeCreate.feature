f4f228f1b3e4eb1ca93cb98b4ca58399e6e4b129
Jens Schulze <jens.schulze@commercetools.de>
2015-05-13 15:44:48 +0200

    add DiscountCodeCreateRequest

---
+++ features/request/DiscountCode/DiscountCodeCreate.feature
@@ -0,0 +1,42 @@
+Feature: I want to create a new discount code
+  Background:
+    Given i have a cartDiscount cartDiscountReference object as cartDiscountReference
+    And the id is cartDiscountId
+    Given i have a cartDiscount cartDiscountReferenceCollection object as discountReferences
+    And add the cartDiscountReference object to discountReferences collection
+    Given i have a discountCode draft
+    And the code is "discount"
+    And the cartDiscounts is discountReferences object
+    And the isActive is 1 as bool
+    And set the name to "myDiscountCode" in en
+    And set the description to "My DiscountCode" in en
+    And set the cartPredicate to test
+    And set the maxApplications to 100 as int
+    And set the maxApplicationsPerCustomer to 1 as int
+
+  Scenario: create a discount code
+    When i want to create a "discountCode"
+    Then the path should be "discount-codes"
+    And the method should be "POST"
+    And the request should be
+    """
+    {
+      "name": {
+        "en": "myDiscountCode"
+      },
+      "description": {
+        "en": "My DiscountCode"
+      },
+      "code": "discount",
+      "cartDiscounts": [
+        {
+          "typeId": "cart-discount",
+          "id": "cartDiscountId"
+        }
+      ],
+      "cartPredicate": "test",
+      "isActive": true,
+      "maxApplications": 100,
+      "maxApplicationsPerCustomer": 1
+    }
+    """
---
+++ src/Model/DiscountCode/DiscountCodeDraft.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * @author @ct-jensschulze <jens.schulze@commercetools.de>
+ */
+
+namespace Sphere\Core\Model\DiscountCode;
+
+use Sphere\Core\Model\CartDiscount\CartDiscountReferenceCollection;
+use Sphere\Core\Model\Common\Context;
+use Sphere\Core\Model\Common\JsonObject;
+use Sphere\Core\Model\Common\OfTrait;
+use Sphere\Core\Model\Common\LocalizedString;
+
+/**
+ * Class DiscountCodeDraft
+ * @package Sphere\Core\Model\DiscountCode
+ * @method LocalizedString getName()
+ * @method DiscountCodeDraft setName(LocalizedString $name = null)
+ * @method LocalizedString getDescription()
+ * @method DiscountCodeDraft setDescription(LocalizedString $description = null)
+ * @method string getCode()
+ * @method DiscountCodeDraft setCode(string $code = null)
+ * @method CartDiscountReferenceCollection getCartDiscounts()
+ * @method DiscountCodeDraft setCartDiscounts(CartDiscountReferenceCollection $cartDiscounts = null)
+ * @method string getCartPredicate()
+ * @method DiscountCodeDraft setCartPredicate(string $cartPredicate = null)
+ * @method bool getIsActive()
+ * @method DiscountCodeDraft setIsActive(bool $isActive = null)
+ * @method int getMaxApplications()
+ * @method DiscountCodeDraft setMaxApplications(int $maxApplications = null)
+ * @method int getMaxApplicationsPerCustomer()
+ * @method DiscountCodeDraft setMaxApplicationsPerCustomer(int $maxApplicationsPerCustomer = null)
+ */
+class DiscountCodeDraft extends JsonObject
+{
+    use OfTrait;
+
+    public function getFields()
+    {
+        return [
+            'name' => [static::TYPE => '\Sphere\Core\Model\Common\LocalizedString'],
+            'description' => [static::TYPE => '\Sphere\Core\Model\Common\LocalizedString'],
+            'code' => [static::TYPE => 'string'],
+            'cartDiscounts' => [static::TYPE => '\Sphere\Core\Model\CartDiscount\CartDiscountReferenceCollection'],
+            'cartPredicate' => [static::TYPE => 'string'],
+            'isActive' => [static::TYPE => 'bool'],
+            'maxApplications' => [static::TYPE => 'int'],
+            'maxApplicationsPerCustomer' => [static::TYPE => 'int'],
+        ];
+    }
+
+    /**
+     * @param string $code
+     * @param CartDiscountReferenceCollection $cartDiscounts
+     * @param bool $isActive
+     * @param Context|callable $context
+     */
+    public function __construct($code, CartDiscountReferenceCollection $cartDiscounts, $isActive, $context = null)
+    {
+        $this->setContext($context)->setCode($code)->setCartDiscounts($cartDiscounts)->setIsActive($isActive);
+    }
+
+    /**
+     * @param array $data
+     * @param Context|callable $context
+     * @return static
+     */
+    public static function fromArray(array $data, $context = null)
+    {
+        $draft = new static(
+            $data['code'],
+            CartDiscountReferenceCollection::fromArray($data['cartDiscounts'], $context),
+            $data['isActive'],
+            $context
+        );
+        $draft->setRawData($data);
+
+        return $draft;
+    }
+}
