2afea8ad11d15d6144257bef3affa82eae039da5
Jens Schulze <jens.schulze@commercetools.de>
2015-07-17 11:02:53 +0200

    refactor(CurrencyFormatter): add ext-intl to requirements
    This change removes the changing behavior of the CurrencyFormatter if the ext-intl extension is not available.
    
    BREAKING CHANGE: ext-intl is now mandatory

--- tests/unit/Helper/CurrencyFormatterTest.php
+++ tests/unit/Helper/CurrencyFormatterTest.php
@@ -8,54 +8,14 @@ namespace Sphere\Core\Helper;
 
 use Sphere\Core\Model\Common\Context;
 
-function extension_loaded($value)
-{
-    if ($value === 'intl') {
-        return CurrencyFormatterTest::getIntlLoaded();
-    }
-    return \extension_loaded($value);
-}
-
 class CurrencyFormatterTest extends \PHPUnit_Framework_TestCase
 {
-    protected static $intlActive = true;
-
-    public static function getIntlLoaded()
-    {
-        return static::$intlActive;
-    }
-
-    protected function setUp()
-    {
-        static::$intlActive = \extension_loaded('intl');
-    }
-
-
-    protected function tearDown()
-    {
-        parent::tearDown();
-        static::$intlActive = \extension_loaded('intl');
-    }
-
     public function testDefaultFormatter()
     {
         $context = new Context();
         $context->setLocale('en_US');
         $formatter = new CurrencyFormatter($context);
 
-        if (extension_loaded('intl')) {
-            $this->assertSame('$1.00', $formatter->format(100, 'USD'));
-        } else {
-            $this->assertSame('1.00 USD', $formatter->format(100, 'USD'));
-        }
-    }
-
-    public function testDefaultFormatterNoIntl()
-    {
-        static::$intlActive = false;
-        $context = new Context();
-        $context->setLocale('en_US');
-        $formatter = new CurrencyFormatter($context);
-        $this->assertSame('1.00 USD', $formatter->format(100, 'USD'));
+        $this->assertSame('$1.00', $formatter->format(100, 'USD'));
     }
 }
