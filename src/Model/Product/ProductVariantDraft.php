971cfbf43f2202915493dd5196e85b08f3477c2e
Jens Schulze <jens.schulze@commercetools.de>
2015-07-31 11:20:13 +0200

    fix(ProductVariantDraft): add images definition
    Closes #135

--- src/Model/Product/ProductVariantDraft.php
+++ src/Model/Product/ProductVariantDraft.php
@@ -9,6 +9,7 @@ namespace Commercetools\Core\Model\Product;
 use Commercetools\Core\Model\Common\AttributeCollection;
 use Commercetools\Core\Model\Common\JsonObject;
 use Commercetools\Core\Model\Common\PriceCollection;
+use Commercetools\Core\Model\Common\ImageCollection;
 
 /**
  * @package Commercetools\Core\Model\Product
@@ -19,6 +20,8 @@ use Commercetools\Core\Model\Common\PriceCollection;
  * @method ProductVariantDraft setAttributes(AttributeCollection $attributes = null)
  * @method PriceCollection getPrices()
  * @method AttributeCollection getAttributes()
+ * @method ImageCollection getImages()
+ * @method ProductVariantDraft setImages(ImageCollection $images = null)
  */
 class ProductVariantDraft extends JsonObject
 {
@@ -27,6 +30,7 @@ class ProductVariantDraft extends JsonObject
         return [
             'sku' => [self::TYPE => 'string'],
             'prices' => [self::TYPE => '\Commercetools\Core\Model\Common\PriceCollection'],
+            'images' => [static::TYPE => '\Commercetools\Core\Model\Common\ImageCollection'],
             'attributes' => [self::TYPE => '\Commercetools\Core\Model\Common\AttributeCollection'],
         ];
     }
