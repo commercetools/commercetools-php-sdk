2b893d0e5d568e69f2273f30c1a45d76cfed99ea
Jens Schulze <jens.schulze@commercetools.de>
2015-09-02 15:17:27 +0200

    WIP: add getBySlug to catalog collection

--- src/Model/Category/CategoryCollection.php
+++ src/Model/Category/CategoryCollection.php
@@ -15,5 +15,29 @@ use Commercetools\Core\Model\Common\Collection;
  */
 class CategoryCollection extends Collection
 {
+    const SLUG = 'slug';
+
     protected $type = '\Commercetools\Core\Model\Category\Category';
+
+    protected function indexRow($offset, $row)
+    {
+        if ($row instanceof Category) {
+            $slugs = $row->getSlug()->toArray();
+        } else {
+            $slugs = isset($row[static::SLUG]) ? $row[static::SLUG] : [];
+        }
+        foreach ($slugs as $locale => $slug) {
+            $this->addToIndex(static::SLUG, $offset, $locale . '-' . $slug);
+        }
+    }
+
+    /**
+     * @param $locale
+     * @param $slug
+     * @return Category
+     */
+    public function getBySlug($slug, $locale)
+    {
+        return $this->getBy(static::SLUG, $locale . '-' . $slug);
+    }
 }
