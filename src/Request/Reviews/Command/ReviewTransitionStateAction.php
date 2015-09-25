827831352834d39628499353d673d5fcf80f9855
Jens Schulze <jens.schulze@commercetools.de>
2015-09-25 15:14:36 +0200

    feat(Review): add state to review

---
+++ src/Request/Reviews/Command/ReviewTransitionStateAction.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author @ct-jensschulze <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Request\Reviews\Command;
+
+use Commercetools\Core\Request\States\Command\TransitionStateAction;
+use Commercetools\Core\Model\State\StateReference;
+
+/**
+ * @package Commercetools\Core\Request\Reviews\Command
+ *
+ * @method string getAction()
+ * @method ReviewTransitionStateAction setAction(string $action = null)
+ * @method StateReference getState()
+ * @method ReviewTransitionStateAction setState(StateReference $state = null)
+ */
+class ReviewTransitionStateAction extends TransitionStateAction
+{
+
+}
