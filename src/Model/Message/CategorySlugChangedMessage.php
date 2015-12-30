014dde2490dddbe7c834fb4745a8bcc193a867d0
Jens Schulze <jens.schulze@commercetools.de>
2015-12-30 12:16:36 +0100

    feat(Category): add CategoryCreated and CategorySlugChanged messages

---
+++ src/Model/Message/CategorySlugChangedMessage.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Model\Message;
+
+use Commercetools\Core\Model\Common\DateTimeDecorator;
+use Commercetools\Core\Model\Common\Reference;
+use Commercetools\Core\Model\Product\ProductProjection;
+use Commercetools\Core\Model\Common\LocalizedString;
+
+/**
+ * @package Commercetools\Core\Model\Message
+ *
+ * @method string getId()
+ * @method CategorySlugChangedMessage setId(string $id = null)
+ * @method DateTimeDecorator getCreatedAt()
+ * @method CategorySlugChangedMessage setCreatedAt(\DateTime $createdAt = null)
+ * @method int getSequenceNumber()
+ * @method CategorySlugChangedMessage setSequenceNumber(int $sequenceNumber = null)
+ * @method Reference getResource()
+ * @method CategorySlugChangedMessage setResource(Reference $resource = null)
+ * @method int getResourceVersion()
+ * @method CategorySlugChangedMessage setResourceVersion(int $resourceVersion = null)
+ * @method string getType()
+ * @method CategorySlugChangedMessage setType(string $type = null)
+ * @method LocalizedString getSlug()
+ * @method CategorySlugChangedMessage setSlug(LocalizedString $slug = null)
+ */
+class CategorySlugChangedMessage extends Message
+{
+    const MESSAGE_TYPE = 'CategorySlugChanged';
+
+    public function fieldDefinitions()
+    {
+        $definitions = parent::fieldDefinitions();
+        $definitions['slug'] = [static::TYPE => '\Commercetools\Core\Model\Common\LocalizedString'];
+
+        return $definitions;
+    }
+}
