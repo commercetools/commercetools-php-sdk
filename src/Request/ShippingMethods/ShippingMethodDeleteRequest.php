e5510f67d657f465933585b271c364589c11710f
Jens Schulze <jens.schulze@commercetools.de>
2016-02-03 10:30:43 +0100

    feat(ShippingMethod): add delete request for shipping methods

---
+++ src/Request/ShippingMethods/ShippingMethodDeleteRequest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Request\ShippingMethods;
+
+use Commercetools\Core\Model\Common\Context;
+use Commercetools\Core\Model\ShippingMethod\ShippingMethod;
+use Commercetools\Core\Request\AbstractDeleteRequest;
+use Commercetools\Core\Response\ApiResponseInterface;
+
+/**
+ * @package Commercetools\Core\Request\ShippingMethods
+ * @apidoc http://dev.sphere.io/http-api-projects-shippingMethods.html#delete-shipping-method
+ * @method ShippingMethod mapResponse(ApiResponseInterface $response)
+ */
+class ShippingMethodDeleteRequest extends AbstractDeleteRequest
+{
+    protected $resultClass = '\Commercetools\Core\Model\ShippingMethod\ShippingMethod';
+
+    /**
+     * @param string $id
+     * @param int $version
+     * @param Context $context
+     */
+    public function __construct($id, $version, Context $context = null)
+    {
+        parent::__construct(ShippingMethodsEndpoint::endpoint(), $id, $version, $context);
+    }
+
+    /**
+     * @param string $id
+     * @param int $version
+     * @param Context $context
+     * @return static
+     */
+    public static function ofIdAndVersion($id, $version, Context $context = null)
+    {
+        return new static($id, $version, $context);
+    }
+}
