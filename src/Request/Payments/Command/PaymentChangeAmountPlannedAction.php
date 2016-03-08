2815f98e6e08540d3e61164d4a2609342a625e5b
Jens Schulze <jens.schulze@commercetools.de>
2016-03-08 17:31:27 +0100

    feat(Payment): add change amount planned update action

---
+++ src/Request/Payments/Command/PaymentChangeAmountPlannedAction.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Request\Payments\Command;
+
+use Commercetools\Core\Model\Common\Context;
+use Commercetools\Core\Request\AbstractAction;
+use Commercetools\Core\Model\Common\Money;
+
+/**
+ * @package Commercetools\Core\Request\Payments\Command
+ * @link http://dev.commercetools.com/http-api-projects-payments.html#change-amount-planned
+ * @method string getAction()
+ * @method PaymentChangeAmountPlannedAction setAction(string $action = null)
+ * @method Money getAmount()
+ * @method PaymentChangeAmountPlannedAction setAmount(Money $amount = null)
+ */
+class PaymentChangeAmountPlannedAction extends AbstractAction
+{
+    public function fieldDefinitions()
+    {
+        return [
+            'action' => [static::TYPE => 'string'],
+            'amount' => [static::TYPE => '\Commercetools\Core\Model\Common\Money'],
+        ];
+    }
+
+    /**
+     * @param array $data
+     * @param Context|callable $context
+     */
+    public function __construct(array $data = [], $context = null)
+    {
+        parent::__construct($data, $context);
+        $this->setAction('changeAmountPlanned');
+    }
+}
