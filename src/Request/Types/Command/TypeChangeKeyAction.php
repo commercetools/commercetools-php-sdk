1c2ebf455454e5d2e58d1633beacf02b6efba08d
Jens Schulze <jens.schulze@commercetools.de>
2016-03-11 14:22:19 +0100

    feat(Type): add type change key action

---
+++ src/Request/Types/Command/TypeChangeKeyAction.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Request\Types\Command;
+
+use Commercetools\Core\Model\Common\Context;
+use Commercetools\Core\Request\AbstractAction;
+use Commercetools\Core\Model\Common\LocalizedString;
+
+/**
+ * @package Commercetools\Core\Request\Types\Command
+ * @link https://dev.commercetools.com/http-api-projects-types.html#change-key
+ * @method string getAction()
+ * @method TypeChangeKeyAction setAction(string $action = null)
+ * @method string getKey()
+ * @method TypeChangeKeyAction setKey(string $key = null)
+ */
+class TypeChangeKeyAction extends AbstractAction
+{
+    public function fieldDefinitions()
+    {
+        return [
+            'action' => [static::TYPE => 'string'],
+            'key' => [static::TYPE => 'string'],
+        ];
+    }
+
+    /**
+     * @param array $data
+     * @param Context|callable $context
+     */
+    public function __construct(array $data = [], $context = null)
+    {
+        parent::__construct($data, $context);
+        $this->setAction('changeKey');
+    }
+
+    /**
+     * @param string $key
+     * @param Context|callable $context
+     * @return TypeChangeKeyAction
+     */
+    public static function ofKey($key, $context = null)
+    {
+        return static::of($context)->setKey($key);
+    }
+}
