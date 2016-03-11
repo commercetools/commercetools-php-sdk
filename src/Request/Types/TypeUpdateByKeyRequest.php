72e4bd22e1672d49ef0ff550b01abb926c3261cb
Jens Schulze <jens.schulze@commercetools.de>
2016-03-11 14:21:27 +0100

    feat(Type): add type update by key request

---
+++ src/Request/Types/TypeUpdateByKeyRequest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Request\Types;
+
+use Commercetools\Core\Model\Common\Context;
+use Commercetools\Core\Request\AbstractUpdateByKeyRequest;
+use Commercetools\Core\Model\Type\Type;
+use Commercetools\Core\Response\ApiResponseInterface;
+
+/**
+ * @package Commercetools\Core\Request\Types
+ * @link https://dev.commercetools.com/http-api-projects-types.html#update-type-by-key
+ * @method Type mapResponse(ApiResponseInterface $response)
+ */
+class TypeUpdateByKeyRequest extends AbstractUpdateByKeyRequest
+{
+    protected $resultClass = '\Commercetools\Core\Model\Type\Type';
+
+    /**
+     * @param string $key
+     * @param string $version
+     * @param array $actions
+     * @param Context $context
+     */
+    public function __construct($key, $version, array $actions = [], Context $context = null)
+    {
+        parent::__construct(TypesEndpoint::endpoint(), $key, $version, $actions, $context);
+    }
+
+    /**
+     * @param string $key
+     * @param int $version
+     * @param Context $context
+     * @return static
+     */
+    public static function ofKeyAndVersion($key, $version, Context $context = null)
+    {
+        return new static($key, $version, [], $context);
+    }
+}
