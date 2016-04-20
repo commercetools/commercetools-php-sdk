291bd05613c912387ebd6f802ba238acfb7dee2a
Jens Schulze <jens.schulze@commercetools.de>
2016-04-20 10:59:43 +0200

    feat(Cart): add helper to calculate line item count

---
+++ tests/unit/Model/Cart/CartTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+
+namespace Commercetools\Core\Model\Cart;
+
+
+class CartTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getCarts
+     */
+    public function testEmptyLineItemCount($cart, $quantity)
+    {
+        $cart = Cart::fromArray($cart);
+        $this->assertSame($quantity, $cart->getLineItemCount());
+    }
+
+    public function getCarts()
+    {
+        return [
+            [
+                [],
+                0
+            ],
+            [
+                ['lineItems' => []],
+                0
+            ],
+            [
+                ['lineItems' => [['quantity' => 1]]],
+                1
+            ],
+            [
+                ['lineItems' => [['quantity' => 1], ['quantity' => 3]]],
+                4
+            ],
+        ];
+    }
+}
