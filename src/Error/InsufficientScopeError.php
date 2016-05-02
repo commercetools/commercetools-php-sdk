c6f9f9666b224134195926360154aae8821510b5
Jens Schulze <jens.schulze@commercetools.de>
2016-05-02 11:12:04 +0200

    docs(Error): add doc block for insufficient scope error

--- src/Error/InsufficientScopeError.php
+++ src/Error/InsufficientScopeError.php
@@ -5,6 +5,14 @@
 
 namespace Commercetools\Core\Error;
 
+/**
+ * @package Commercetools\Core\Error
+ *
+ * @method string getCode()
+ * @method InsufficientScopeError setCode(string $code = null)
+ * @method string getMessage()
+ * @method InsufficientScopeError setMessage(string $message = null)
+ */
 class InsufficientScopeError extends ApiError
 {
     const CODE = 'insufficient_scope';
