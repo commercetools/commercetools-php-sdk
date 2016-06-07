5ca934ea5bb2e6d4352a517b1c24c9b0cbabecd1
Jens Schulze <jens.schulze@commercetools.de>
2016-06-07 13:53:48 +0200

    docs(Changelog): update changelog

--- src/AbstractHttpClient.php
+++ src/AbstractHttpClient.php
@@ -14,7 +14,7 @@ use Commercetools\Core\Client\Adapter\AdapterInterface;
  */
 abstract class AbstractHttpClient
 {
-    const VERSION = '1.1';
+    const VERSION = '1.1.1';
 
     /**
      * @var AdapterInterface
