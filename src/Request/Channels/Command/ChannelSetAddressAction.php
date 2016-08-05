66a40a935f9d33cc015cece74db967a23a07be1f
Jens Schulze <jens.schulze@commercetools.de>
2016-08-05 16:05:20 +0200

    feat(Channel): add Address field to Channel
    Closes #237

---
+++ src/Request/Channels/Command/ChannelSetAddressAction.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Request\Channels\Command;
+
+use Commercetools\Core\Model\Common\Context;
+use Commercetools\Core\Request\AbstractAction;
+use Commercetools\Core\Model\Common\Address;
+
+/**
+ * @package Commercetools\Core\Request\Channels\Command
+ * @link http://dev.commercetools.com/http-api-projects-channels.html#set-address
+ * @method string getAction()
+ * @method ChannelSetAddressAction setAction(string $action = null)
+ * @method Address getAddress()
+ * @method ChannelSetAddressAction setAddress(Address $address = null)
+ */
+class ChannelSetAddressAction extends AbstractAction
+{
+    public function fieldDefinitions()
+    {
+        return [
+            'action' => [static::TYPE => 'string'],
+            'address' => [static::TYPE => '\Commercetools\Core\Model\Common\Address'],
+        ];
+    }
+
+    /**
+     * @param array $data
+     * @param Context|callable $context
+     */
+    public function __construct(array $data = [], $context = null)
+    {
+        parent::__construct($data, $context);
+        $this->setAction('setAddress');
+    }
+}
