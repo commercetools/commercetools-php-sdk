8fae7f822070271bd9a681ae91accf7354eab464
Jens Schulze <jens.schulze@commercetools.de>
2016-11-11 10:49:33 +0100

    feat(Subscription): add delivery objects

---
+++ src/Model/Subscription/ResourceCreatedDelivery.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Model\Subscription;
+
+use Commercetools\Core\Model\Common\Reference;
+
+/**
+ * @package Commercetools\Core\Model\Subscription
+ *
+ * @method string getProjectKey()
+ * @method ResourceCreatedDelivery setProjectKey(string $projectKey = null)
+ * @method string getNotificationType()
+ * @method ResourceCreatedDelivery setNotificationType(string $notificationType = null)
+ * @method Reference getReference()
+ * @method ResourceCreatedDelivery setReference(Reference $reference = null)
+ * @method int getVersion()
+ * @method ResourceCreatedDelivery setVersion(int $version = null)
+ */
+class ResourceCreatedDelivery extends Delivery
+{
+    public function fieldDefinitions()
+    {
+        $definition = parent::fieldDefinitions();
+        $definition['version'] = [static::TYPE => 'int'];
+        return $definition;
+    }
+}
---
+++ src/Model/Subscription/ResourceDeletedDelivery.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Model\Subscription;
+
+use Commercetools\Core\Model\Common\Reference;
+
+/**
+ * @package Commercetools\Core\Model\Subscription
+ *
+ * @method string getProjectKey()
+ * @method ResourceDeletedDelivery setProjectKey(string $projectKey = null)
+ * @method string getNotificationType()
+ * @method ResourceDeletedDelivery setNotificationType(string $notificationType = null)
+ * @method Reference getReference()
+ * @method ResourceDeletedDelivery setReference(Reference $reference = null)
+ * @method int getVersion()
+ * @method ResourceDeletedDelivery setVersion(int $version = null)
+ */
+class ResourceDeletedDelivery extends Delivery
+{
+    public function fieldDefinitions()
+    {
+        $definition = parent::fieldDefinitions();
+        $definition['version'] = [static::TYPE => 'int'];
+        return $definition;
+    }
+}
