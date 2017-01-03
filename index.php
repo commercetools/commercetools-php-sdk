1f68835dea0f154f46de6c28ea6ab42bd45a9fec
Jens Schulze <jens.schulze@commercetools.de>
2017-01-03 10:58:10 +0100

    WIP: prepare building of a phar distribution of the SDK

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * This is a bootstrap file for a phar distribution of the SDK in order to be able to use
+ * `require 'commercetools-php-sdk.phar';`
+ */
+require __DIR__ . '/vendor/autoload.php';
