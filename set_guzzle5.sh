6e3a55a6c7c6022deaf57637a73de20d5fcf34d9
Jens Schulze <jens.schulze@commercetools.de>
2017-01-03 14:21:25 +0100

    WIP: fix minimum guzzle version for travis

--- set_guzzle5.sh
+++ set_guzzle5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SRC='"guzzlehttp/guzzle": "^6.0"'
-DST='"guzzlehttp/guzzle": "^5.0", "guzzlehttp/log-subscriber": "^1.0", "guzzlehttp/ringphp": "^1.0.7"'
+DST='"guzzlehttp/guzzle": "^5.3.1", "guzzlehttp/log-subscriber": "^1.0", "guzzlehttp/ringphp": "^1.0.7"'
 sed -ibak -e "s|$SRC|$DST|g" composer.json
