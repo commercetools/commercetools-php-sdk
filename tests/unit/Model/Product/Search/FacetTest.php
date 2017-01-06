8e0c946a424ced308c09399f6d1b7718e4e92929
Jens Schulze <jens.schulze@commercetools.de>
2017-01-06 16:56:54 +0100

    feat(ProductSearch): add productCount to FacetResult
    Closes #279

---
+++ tests/unit/Model/Product/Search/FacetTest.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+
+namespace Commercetools\Core\Model\Product\Search;
+
+
+class FacetTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNumeric()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue(10);
+        $this->assertSame('test:10', (string)$filter);
+    }
+
+    public function testIntString()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue('10');
+        $this->assertSame('test:"10"', (string)$filter);
+    }
+
+    public function testIntStringArray()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue(['1', '2', '3']);
+        $this->assertSame('test:"1","2","3"', (string)$filter);
+    }
+
+    public function testFloatStringArray()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue(['1.1', '2.2', '3.3']);
+        $this->assertSame('test:"1.1","2.2","3.3"', (string)$filter);
+    }
+
+    public function testAlias()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue(10)->setAlias('foo');
+        $this->assertSame('test:10 as foo', (string)$filter);
+    }
+
+    public function testString()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue('key')->setAlias('foo');
+        $this->assertSame('test:"key" as foo', (string)$filter);
+    }
+
+    public function testIntArray()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue([1, 2, 3])->setAlias('foo');
+        $this->assertSame('test:1,2,3 as foo', (string)$filter);
+    }
+
+    public function testFloatArray()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue([1.1, 2.2, 3.3])->setAlias('foo');
+        $this->assertSame('test:1.1,2.2,3.3 as foo', (string)$filter);
+    }
+
+    public function testSingleStringInArray()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue(["test"])->setAlias('foo');
+        $this->assertSame('test:"test" as foo', (string)$filter);
+    }
+
+    public function testRange()
+    {
+        $filter = Filter::ofName('test');
+
+        $filterRangeCollection = FilterRangeCollection::of()
+            ->add(FilterRange::ofFromAndTo(1, 10))
+            ->add(FilterRange::ofFromAndTo(11, 20))
+        ;
+        $filter->setValue($filterRangeCollection)->setAlias('foo');
+        $this->assertSame('test:range(1 to 10),(11 to 20) as foo', (string)$filter);
+    }
+
+    public function testBoolTrue()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue(true);
+        $this->assertSame('test:true', (string)$filter);
+    }
+
+    public function testBoolFalse()
+    {
+        $filter = Filter::ofName('test');
+        $filter->setValue(false);
+        $this->assertSame('test:false', (string)$filter);
+    }
+
+    public function testFacetCountingProducts()
+    {
+        $facet = Facet::ofName('test')->countingProducts(true);
+        $this->assertSame('test counting products', (string)$facet);
+    }
+
+    public function testFacetCountingProductsWithValue()
+    {
+        $facet = Facet::ofName('test')->setValue('red')->countingProducts(true);
+        $this->assertSame('test:"red" counting products', (string)$facet);
+    }
+
+    public function testFacetNotCountingProducts()
+    {
+        $facet = Facet::ofName('test')->countingProducts(false);
+        $this->assertSame('test', (string)$facet);
+    }
+}
