52932d84ebeac73ccac27c37a2ba04c32bf7f868
Jens Schulze <jens.schulze@commercetools.de>
2018-02-23 10:12:29 +0100

    feat(ProductType): support changeAttributeConstraint update action
    Closes #370

---
+++ src/Core/Request/ProductTypes/Command/ProductTypeChangeAttributeConstraintAction.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @author @jenschude <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Request\ProductTypes\Command;
+
+use Commercetools\Core\Model\Common\Context;
+use Commercetools\Core\Request\AbstractAction;
+
+/**
+ * @package Commercetools\Core\Request\ProductTypes\Command
+ * @link https://docs.commercetools.com/http-api-projects-productTypes.html#change-attributedefinition-attributeconstraint
+ * @method string getAction()
+ * @method ProductTypeChangeAttributeConstraintAction setAction(string $action = null)
+ * @method string getAttributeName()
+ * @method ProductTypeChangeAttributeConstraintAction setAttributeName(string $attributeName = null)
+ * @method string getNewValue()
+ * @method ProductTypeChangeAttributeConstraintAction setNewValue(string $newValue = null)
+ */
+class ProductTypeChangeAttributeConstraintAction extends AbstractAction
+{
+    public function fieldDefinitions()
+    {
+        return [
+            'action' => [static::TYPE => 'string'],
+            'attributeName' => [static::TYPE => 'string'],
+            'newValue' => [static::TYPE => 'string']
+        ];
+    }
+
+    /**
+     * @param array $data
+     * @param Context|callable $context
+     */
+    public function __construct(array $data = [], $context = null)
+    {
+        parent::__construct($data, $context);
+        $this->setAction('changeAttributeConstraint');
+    }
+
+    /**
+     * @param string $attributeName
+     * @param string $attributeConstraint
+     * @param Context|callable $context
+     * @return ProductTypeChangeAttributeConstraintAction
+     */
+    public static function ofAttributeNameAndAttributeConstraint($attributeName, $attributeConstraint, $context = null)
+    {
+        return static::of($context)->setAttributeName($attributeName)->setNewValue($attributeConstraint);
+    }
+}
