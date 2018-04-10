2f8c91d9d89a377705d1f5bb6467d86016b2c909
Jens Schulze <jens.schulze@commercetools.de>
2018-04-10 11:56:56 +0200

    feat(Message): add customer messages
    Closes #391

---
+++ src/Core/Model/Message/CustomerEmailVerifiedMessage.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author @jenschude <jens.schulze@commercetools.de>
+ */
+
+namespace Commercetools\Core\Model\Message;
+
+use Commercetools\Core\Model\Common\DateTimeDecorator;
+use DateTime;
+use Commercetools\Core\Model\Common\Reference;
+
+/**
+ * @package Commercetools\Core\Model\Message
+ * @link https://docs.commercetools.com/http-api-projects-messages.html#customeremailverified-message
+ * @method string getId()
+ * @method CustomerEmailVerifiedMessage setId(string $id = null)
+ * @method int getVersion()
+ * @method CustomerEmailVerifiedMessage setVersion(int $version = null)
+ * @method DateTimeDecorator getCreatedAt()
+ * @method CustomerEmailVerifiedMessage setCreatedAt(DateTime $createdAt = null)
+ * @method DateTimeDecorator getLastModifiedAt()
+ * @method CustomerEmailVerifiedMessage setLastModifiedAt(DateTime $lastModifiedAt = null)
+ * @method int getSequenceNumber()
+ * @method CustomerEmailVerifiedMessage setSequenceNumber(int $sequenceNumber = null)
+ * @method Reference getResource()
+ * @method CustomerEmailVerifiedMessage setResource(Reference $resource = null)
+ * @method int getResourceVersion()
+ * @method CustomerEmailVerifiedMessage setResourceVersion(int $resourceVersion = null)
+ * @method string getType()
+ * @method CustomerEmailVerifiedMessage setType(string $type = null)
+ */
+class CustomerEmailVerifiedMessage extends Message
+{
+    const MESSAGE_TYPE = 'CustomerEmailVerified';
+}
