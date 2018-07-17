6a92d61c740086d9cbbc9e3c1ccb65f07ec722e1
Jens Schulze <jenschude@gmail.com>
2018-07-17 15:18:14 +0200

    WIP: add user-agent

--- tests/functional/HttpClientTest.php
+++ tests/functional/HttpClientTest.php
@@ -5,6 +5,7 @@
 
 namespace Commercetools\Core\Test\Functional;
 
+use Commercetools\Core\AbstractHttpClient;
 use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
@@ -15,13 +16,15 @@ class HttpClientTest extends TestCase
      */
     public function testTLSFail()
     {
+        $headers = ['User-Agent' => $this->getUserAgent()];
         $client = new Client();
         $client->get(
             'https://api.escemo.com',
             [
                 'curl' => [
                     CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_1
-                ]
+                ],
+                'headers' => $headers
             ]
         );
     }
@@ -29,14 +32,28 @@ class HttpClientTest extends TestCase
     public function testTLSSuccess()
     {
         $client = new Client();
+        $headers = ['User-Agent' => $this->getUserAgent()];
         $reponse = $client->get(
             'https://api.escemo.com',
             [
                 'curl' => [
                     CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2
-                ]
+                ],
+                'headers' => $headers
             ]
         );
         $this->assertSame(200, $reponse->getStatusCode());
+    }
+
+    protected function getUserAgent()
+    {
+        $agent = 'commercetools-php-sdk/' . AbstractHttpClient::VERSION;
+
+        $agent .= ' (Guzzle/' . Client::VERSION;
+        if (extension_loaded('curl') && function_exists('curl_version')) {
+            $agent .= '; curl/' . \curl_version()['version'];
+        }
+        $agent .= ') PHP/' . PHP_VERSION;
+        return $agent;
     }
 }
