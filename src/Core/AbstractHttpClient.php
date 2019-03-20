e6c48b7172e4ad5bf082f6dfd6abe2163839cc62
Jens Schulze <jenschude@gmail.com>
2019-03-20 13:47:40 +0100

    docs(Client): update client version

--- src/Core/AbstractHttpClient.php
+++ src/Core/AbstractHttpClient.php
@@ -14,7 +14,7 @@ use Commercetools\Core\Client\Adapter\AdapterInterface;
  */
 abstract class AbstractHttpClient
 {
-    const VERSION = '2.7.0';
+    const VERSION = '2.8.0-dev';
 
     /**
      * @var AdapterInterface
