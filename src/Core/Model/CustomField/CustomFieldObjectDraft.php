21df74f99e245cadd08dad4c1dfe95465e3d107f
Barbara Palumbo <barbara.palumbo@commercetools.de>
2019-11-21 17:26:48 +0100

    WIP: fix error in testing

--- src/Core/Model/CustomField/CustomFieldObjectDraft.php
+++ src/Core/Model/CustomField/CustomFieldObjectDraft.php
@@ -21,7 +21,7 @@ use Commercetools\Core\Model\Common\ResourceIdentifier;
 class CustomFieldObjectDraft extends CustomFieldObject
 {
     /**
-     * @param $typeKey
+     * @param string $typeKey
      * @param Context|callable $context
      * @return CustomFieldObjectDraft
      */
