ee7620fcdb240443b5d4ca0227d2f1cf51e57c97
Barbara Palumbo <barbara.palumbo@commercetools.de>
2019-11-28 13:58:11 +0100

    feat(InStoreRequest): add request to InStoreRequests to add new shipping method endpoint

--- src/Core/Request/InStores/InStoreRequests.php
+++ src/Core/Request/InStores/InStoreRequests.php
@@ -42,6 +42,10 @@ use Commercetools\Core\Request\Orders\OrderDeleteRequest;
 use Commercetools\Core\Request\Orders\OrderQueryRequest;
 use Commercetools\Core\Request\Orders\OrderUpdateByOrderNumberRequest;
 use Commercetools\Core\Request\Orders\OrderUpdateRequest;
+use Commercetools\Core\Request\ShippingMethods\ShippingMethodByCartIdGetRequest;
+use Commercetools\Core\Request\ShippingMethods\ShippingMethodByIdGetRequest;
+use Commercetools\Core\Request\ShippingMethods\ShippingMethodCreateRequest;
+use Commercetools\Core\Request\ShippingMethods\ShippingMethodDeleteRequest;
 
 class InStoreRequests
 {
@@ -83,6 +87,7 @@ class InStoreRequests
         CustomerEmailConfirmRequest::class => 1,
         CustomerUpdateRequest::class => 1,
         CustomerUpdateByKeyRequest::class => 1,
+        ShippingMethodByCartIdGetRequest::class => 1,
     ];
 
     public function can($request)
