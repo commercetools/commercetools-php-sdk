aa55d71679e990f1c28a5731d558b18c7415f832
Jens Schulze <jenschude@gmail.com>
2019-12-02 10:39:52 +0100

    docs(Changelog): update changelog

--- src/Core/AbstractHttpClient.php
+++ src/Core/AbstractHttpClient.php
@@ -14,7 +14,7 @@ use Commercetools\Core\Client\Adapter\AdapterInterface;
  */
 abstract class AbstractHttpClient
 {
-    const VERSION = '2.9.0-dev';
+    const VERSION = '2.9.0';
 
     /**
      * @var AdapterInterface
