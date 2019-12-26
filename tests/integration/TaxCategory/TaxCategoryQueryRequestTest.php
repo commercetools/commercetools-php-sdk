1b6888ec279782e7a66dd9768a3b02f13fbec609
Barbara Palumbo <barbara.palumbo@commercetools.de>
2019-12-26 14:13:34 +0100

    WIP: migrate taxCategory

--- tests/integration/TaxCategory/TaxCategoryQueryRequestTest.php
+++ tests/integration/TaxCategory/TaxCategoryQueryRequestTest.php
@@ -3,109 +3,89 @@
  * @author @jenschude <jens.schulze@commercetools.de>
  */
 
-
 namespace Commercetools\Core\IntegrationTests\TaxCategory;
 
+use Commercetools\Core\Builder\Request\RequestBuilder;
 use Commercetools\Core\IntegrationTests\ApiTestCase;
 use Commercetools\Core\Model\TaxCategory\TaxCategory;
 use Commercetools\Core\Model\TaxCategory\TaxCategoryDraft;
-use Commercetools\Core\Model\TaxCategory\TaxRate;
-use Commercetools\Core\Model\TaxCategory\TaxRateCollection;
-use Commercetools\Core\Request\TaxCategories\TaxCategoryByIdGetRequest;
-use Commercetools\Core\Request\TaxCategories\TaxCategoryByKeyGetRequest;
-use Commercetools\Core\Request\TaxCategories\TaxCategoryCreateRequest;
-use Commercetools\Core\Request\TaxCategories\TaxCategoryDeleteByKeyRequest;
-use Commercetools\Core\Request\TaxCategories\TaxCategoryDeleteRequest;
-use Commercetools\Core\Request\TaxCategories\TaxCategoryQueryRequest;
 
 class TaxCategoryQueryRequestTest extends ApiTestCase
 {
-    /**
-     * @return TaxCategoryDraft
-     */
-    protected function getDraft()
-    {
-        $draft = TaxCategoryDraft::ofNameAndRates(
-            'test-' . $this->getTestRun() . '-name',
-            TaxRateCollection::of()->add(
-                TaxRate::of()->setName('test-' . $this->getTestRun() . '-name')
-                    ->setAmount(0.2)
-                    ->setIncludedInPrice(true)
-                    ->setCountry('DE')
-                    ->setState($this->getRegion())
-            )
-        );
-
-        return $draft;
-    }
-
-    protected function createTaxCategory(TaxCategoryDraft $draft)
-    {
-        $request = TaxCategoryCreateRequest::ofDraft($draft);
-        $response = $request->executeWithClient($this->getClient());
-        $taxCategory = $request->mapResponse($response);
-
-        $this->cleanupRequests[] = TaxCategoryDeleteRequest::ofIdAndVersion(
-            $taxCategory->getId(),
-            $taxCategory->getVersion()
-        );
-
-        return $taxCategory;
-    }
-
     public function testQuery()
     {
-        $draft = $this->getDraft();
-        $taxCategory = $this->createTaxCategory($draft);
+        $client = $this->getApiClient();
 
-        $request = TaxCategoryQueryRequest::of()->where('name="' . $draft->getName() . '"');
-        $response = $request->executeWithClient($this->getClient());
-        $result = $request->mapResponse($response);
+        TaxCategoryFixture::withTaxCategory(
+            $client,
+            function (TaxCategory $taxCategory) use ($client) {
+                $request = RequestBuilder::of()->taxCategories()->query()
+                    ->where('name=:name', ['name' => $taxCategory->getName()]);
+                $response = $this->execute($client, $request);
+                $result = $request->mapFromResponse($response);
 
-        $this->assertCount(1, $result);
-        $this->assertInstanceOf(TaxCategory::class, $result->getAt(0));
-        $this->assertSame($taxCategory->getId(), $result->getAt(0)->getId());
+                $this->assertCount(1, $result);
+                $this->assertInstanceOf(TaxCategory::class, $result->current());
+                $this->assertSame($taxCategory->getId(), $result->current()->getId());
+            }
+        );
     }
 
     public function testGetById()
     {
-        $draft = $this->getDraft();
-        $taxCategory = $this->createTaxCategory($draft);
+        $client = $this->getApiClient();
 
-        $request = TaxCategoryByIdGetRequest::ofId($taxCategory->getId());
-        $response = $request->executeWithClient($this->getClient());
-        $result = $request->mapResponse($response);
+        TaxCategoryFixture::withTaxCategory(
+            $client,
+            function (TaxCategory $taxCategory) use ($client) {
+                $request = RequestBuilder::of()->taxCategories()->getById($taxCategory->getId());
+                $response = $this->execute($client, $request);
+                $result = $request->mapFromResponse($response);
 
-        $this->assertInstanceOf(TaxCategory::class, $taxCategory);
-        $this->assertSame($taxCategory->getId(), $result->getId());
+                $this->assertInstanceOf(TaxCategory::class, $taxCategory);
+                $this->assertSame($taxCategory->getId(), $result->getId());
+            }
+        );
     }
 
     public function testGetByKey()
     {
-        $draft = $this->getDraft()->setKey($this->getTestRun());
-        $taxCategory = $this->createTaxCategory($draft);
+        $client = $this->getApiClient();
 
-        $request = TaxCategoryByKeyGetRequest::ofKey($taxCategory->getKey());
-        $response = $request->executeWithClient($this->getClient());
-        $result = $request->mapResponse($response);
+        TaxCategoryFixture::withDraftTaxCategory(
+            $client,
+            function (TaxCategoryDraft $draft) {
+                return $draft->setKey('set-key');
+            },
+            function (TaxCategory $taxCategory) use ($client) {
+                $request = RequestBuilder::of()->taxCategories()->getByKey($taxCategory->getKey());
+                $response = $this->execute($client, $request);
+                $result = $request->mapFromResponse($response);
 
-        $this->assertInstanceOf(TaxCategory::class, $taxCategory);
-        $this->assertSame($taxCategory->getId(), $result->getId());
+                $this->assertInstanceOf(TaxCategory::class, $taxCategory);
+                $this->assertSame($taxCategory->getId(), $result->getId());
+                $this->assertSame($taxCategory->getKey(), $result->getKey());
+            }
+        );
     }
 
 
     public function testDeleteByKey()
     {
-        $draft = $this->getDraft()->setKey($this->getTestRun());
-        $taxCategory = $this->createTaxCategory($draft);
+        $client = $this->getApiClient();
 
-        $request = TaxCategoryDeleteByKeyRequest::ofKeyAndVersion(
-            $taxCategory->getKey(),
-            $taxCategory->getVersion()
+        TaxCategoryFixture::withDraftTaxCategory(
+            $client,
+            function (TaxCategoryDraft $draft) {
+                return $draft->setKey($this->getTestRun());
+            },
+            function (TaxCategory $taxCategory) use ($client) {
+                $request = RequestBuilder::of()->taxCategories()->deleteByKey($taxCategory);
+                $response = $this->execute($client, $request);
+                $result = $request->mapFromResponse($response);
+
+                $this->assertSame($taxCategory->getId(), $result->getId());
+            }
         );
-        $result = $this->getClient()->execute($request);
-        $response = $request->mapFromResponse($result);
-
-        $this->assertSame($taxCategory->getId(), $response->getId());
     }
 }
