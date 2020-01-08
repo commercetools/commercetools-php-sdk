ffe839e04d304780eae0aeb34fac610e12c56018
Barbara Palumbo <barbara.palumbo@commercetools.de>
2020-01-08 16:51:09 +0100

    WIP: migrate ProductDiscount

--- tests/integration/ProductDiscount/ProductDiscountQueryRequestTest.php
+++ tests/integration/ProductDiscount/ProductDiscountQueryRequestTest.php
@@ -3,96 +3,69 @@
  * @author @jenschude <jens.schulze@commercetools.de>
  */
 
-
 namespace Commercetools\Core\IntegrationTests\ProductDiscount;
 
+use Commercetools\Core\Builder\Request\RequestBuilder;
 use Commercetools\Core\IntegrationTests\ApiTestCase;
-use Commercetools\Core\IntegrationTests\TestHelper;
-use Commercetools\Core\Model\Common\LocalizedString;
-use Commercetools\Core\Model\Common\Money;
-use Commercetools\Core\Model\Common\MoneyCollection;
 use Commercetools\Core\Model\ProductDiscount\ProductDiscount;
 use Commercetools\Core\Model\ProductDiscount\ProductDiscountDraft;
-use Commercetools\Core\Model\ProductDiscount\ProductDiscountValue;
-use Commercetools\Core\Request\ProductDiscounts\ProductDiscountByIdGetRequest;
-use Commercetools\Core\Request\ProductDiscounts\ProductDiscountByKeyGetRequest;
-use Commercetools\Core\Request\ProductDiscounts\ProductDiscountCreateRequest;
-use Commercetools\Core\Request\ProductDiscounts\ProductDiscountDeleteRequest;
-use Commercetools\Core\Request\ProductDiscounts\ProductDiscountQueryRequest;
 
 class ProductDiscountQueryRequestTest extends ApiTestCase
 {
-    /**
-     * @return ProductDiscountDraft
-     */
-    protected function getDraft()
-    {
-        $draft = ProductDiscountDraft::ofNameDiscountPredicateOrderAndActive(
-            LocalizedString::ofLangAndText('en', 'test-' . $this->getTestRun() . '-discount'),
-            ProductDiscountValue::of()->setType('absolute')->setMoney(
-                MoneyCollection::of()
-                    ->add(Money::ofCurrencyAndAmount('EUR', 100))
-            ),
-            '1=1',
-            '0.9' . trim((string)mt_rand(1, TestHelper::RAND_MAX), '0'),
-            false
-        );
-
-        return $draft;
-    }
-
-    protected function createProductDiscount(ProductDiscountDraft $draft)
-    {
-        $request = ProductDiscountCreateRequest::ofDraft($draft);
-        $response = $request->executeWithClient($this->getClient());
-        $productDiscount = $request->mapResponse($response);
-
-        $this->cleanupRequests[] = ProductDiscountDeleteRequest::ofIdAndVersion(
-            $productDiscount->getId(),
-            $productDiscount->getVersion()
-        );
-
-        return $productDiscount;
-    }
-
     public function testQuery()
     {
-        $draft = $this->getDraft();
-        $productDiscount = $this->createProductDiscount($draft);
+        $client = $this->getApiClient();
 
-        $request = ProductDiscountQueryRequest::of()->where('name(en="' . $draft->getName()->en . '")');
-        $response = $request->executeWithClient($this->getClient());
-        $result = $request->mapResponse($response);
+        ProductDiscountFixture::withProductDiscount(
+            $client,
+            function (ProductDiscount $productDiscount) use ($client) {
+                $request = RequestBuilder::of()->productDiscounts()->query()
+                    ->where('name(en=:name)', ['name' => $productDiscount->getName()->en]);
+                $response = $this->execute($client, $request);
+                $result = $request->mapFromResponse($response);
 
-        $this->assertCount(1, $result);
-        $this->assertInstanceOf(ProductDiscount::class, $result->getAt(0));
-        $this->assertSame($productDiscount->getId(), $result->getAt(0)->getId());
+                $this->assertCount(1, $result);
+                $this->assertInstanceOf(ProductDiscount::class, $result->current());
+                $this->assertSame($productDiscount->getId(), $result->current()->getId());
+            }
+        );
     }
 
     public function testGetById()
     {
-        $draft = $this->getDraft();
-        $productDiscount = $this->createProductDiscount($draft);
+        $client = $this->getApiClient();
 
-        $request = ProductDiscountByIdGetRequest::ofId($productDiscount->getId());
-        $response = $request->executeWithClient($this->getClient());
-        $result = $request->mapResponse($response);
+        ProductDiscountFixture::withProductDiscount(
+            $client,
+            function (ProductDiscount $productDiscount) use ($client) {
+                $request = RequestBuilder::of()->productDiscounts()->getById($productDiscount->getId());
+                $response = $this->execute($client, $request);
+                $result = $request->mapFromResponse($response);
 
-        $this->assertInstanceOf(ProductDiscount::class, $productDiscount);
-        $this->assertSame($productDiscount->getId(), $result->getId());
+                $this->assertInstanceOf(ProductDiscount::class, $productDiscount);
+                $this->assertSame($productDiscount->getId(), $result->getId());
+            }
+        );
     }
 
     public function testGetByKey()
     {
-        $draft = $this->getDraft()->setKey('key-' . $this->getTestRun());
-        $productDiscount = $this->createProductDiscount($draft);
+        $client = $this->getApiClient();
 
-        $request = ProductDiscountByKeyGetRequest::ofKey($productDiscount->getKey());
-        $response = $request->executeWithClient($this->getClient());
-        $result = $request->mapResponse($response);
+        ProductDiscountFixture::withDraftProductDiscount(
+            $client,
+            function (ProductDiscountDraft $draft) {
+                return $draft->setKey('key-' . ProductDiscountFixture::uniqueProductDiscountString());
+            },
+            function (ProductDiscount $productDiscount) use ($client) {
+                $request = RequestBuilder::of()->productDiscounts()->getByKey($productDiscount->getKey());
+                $response = $this->execute($client, $request);
+                $result = $request->mapFromResponse($response);
 
-        $this->assertInstanceOf(ProductDiscount::class, $productDiscount);
-        $this->assertSame($productDiscount->getId(), $result->getId());
-        $this->assertSame($productDiscount->getKey(), $result->getKey());
+                $this->assertInstanceOf(ProductDiscount::class, $productDiscount);
+                $this->assertSame($productDiscount->getId(), $result->getId());
+                $this->assertSame($productDiscount->getKey(), $result->getKey());
+            }
+        );
     }
 }
