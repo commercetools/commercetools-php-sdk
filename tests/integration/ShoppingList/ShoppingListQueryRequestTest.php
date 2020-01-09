b657d1b29029d9e9fb8b688c66010ceabb070dac
Barbara Palumbo <barbara.palumbo@commercetools.de>
2020-01-09 14:19:05 +0100

    test(ShoppingList): migrate shoppingList tests

--- tests/integration/ShoppingList/ShoppingListQueryRequestTest.php
+++ tests/integration/ShoppingList/ShoppingListQueryRequestTest.php
@@ -3,89 +3,65 @@
  * @author @jenschude <jens.schulze@commercetools.de>
  */
 
-
 namespace Commercetools\Core\IntegrationTests\ShoppingList;
 
+use Commercetools\Core\Builder\Request\RequestBuilder;
 use Commercetools\Core\IntegrationTests\ApiTestCase;
-use Commercetools\Core\Model\Common\LocalizedString;
 use Commercetools\Core\Model\ShoppingList\ShoppingList;
-use Commercetools\Core\Model\ShoppingList\ShoppingListDraft;
-use Commercetools\Core\Request\ShoppingLists\ShoppingListByIdGetRequest;
-use Commercetools\Core\Request\ShoppingLists\ShoppingListByKeyGetRequest;
-use Commercetools\Core\Request\ShoppingLists\ShoppingListCreateRequest;
-use Commercetools\Core\Request\ShoppingLists\ShoppingListDeleteRequest;
-use Commercetools\Core\Request\ShoppingLists\ShoppingListQueryRequest;
 
 class ShoppingListQueryRequestTest extends ApiTestCase
 {
-    /**
-     * @return ShoppingListDraft
-     */
-    protected function getDraft()
-    {
-        $draft = ShoppingListDraft::ofNameAndKey(
-            LocalizedString::ofLangAndText('en', 'test-' . $this->getTestRun() . '-name'),
-            'key-' . $this->getTestRun()
-        );
-
-        return $draft;
-    }
-
-    protected function createShoppingList(ShoppingListDraft $draft)
-    {
-        /**
-         * @var ShoppingList $shoppingList
-         */
-        $request = ShoppingListCreateRequest::ofDraft($draft);
-        $response = $request->executeWithClient($this->getClient());
-
-        $shoppingList = $request->mapResponse($response);
-
-        $this->cleanupRequests[] = $this->deleteRequest = ShoppingListDeleteRequest::ofIdAndVersion(
-            $shoppingList->getId(),
-            $shoppingList->getVersion()
-        );
-
-        return $shoppingList;
-    }
-
     public function testQuery()
     {
-        $draft = $this->getDraft();
-        $shoppingList = $this->createShoppingList($draft);
+        $client = $this->getApiClient();
 
-        $request = ShoppingListQueryRequest::of()->where('key="' . $draft->getKey() . '"');
-        $response = $request->executeWithClient($this->getClient());
-        $result = $request->mapResponse($response);
+        ShoppingListFixture::withShoppingList(
+            $client,
+            function (ShoppingList $shoppingList) use ($client) {
+                $request = RequestBuilder::of()->shoppingLists()->query()
+                    ->where('key=:key', ['key' => $shoppingList->getKey()]);
+                $response = $this->execute($client, $request);
+                $result = $request->mapFromResponse($response);
 
-        $this->assertCount(1, $result);
-        $this->assertInstanceOf(ShoppingList::class, $result->current());
-        $this->assertSame($shoppingList->getId(), $result->current()->getId());
+                $this->assertCount(1, $result);
+                $this->assertInstanceOf(ShoppingList::class, $result->current());
+                $this->assertSame($shoppingList->getId(), $result->current()->getId());
+            }
+        );
     }
 
     public function testGetById()
     {
-        $draft = $this->getDraft();
-        $shoppingList = $this->createShoppingList($draft);
+        $client = $this->getApiClient();
 
-        $request = ShoppingListByIdGetRequest::ofId($shoppingList->getId());
-        $response = $request->executeWithClient($this->getClient());
-        $result = $request->mapResponse($response);
+        ShoppingListFixture::withShoppingList(
+            $client,
+            function (ShoppingList $shoppingList) use ($client) {
+                $request = RequestBuilder::of()->shoppingLists()->getById($shoppingList->getId());
+                $response = $this->execute($client, $request);
+                $result = $request->mapFromResponse($response);
 
-        $this->assertInstanceOf(ShoppingList::class, $shoppingList);
-        $this->assertSame($shoppingList->getId(), $result->getId());
+                $this->assertInstanceOf(ShoppingList::class, $shoppingList);
+                $this->assertSame($shoppingList->getId(), $result->getId());
+            }
+        );
     }
 
     public function testGetByKey()
     {
-        $draft = $this->getDraft();
-        $shoppingList = $this->createShoppingList($draft);
+        $client = $this->getApiClient();
 
-        $request = ShoppingListByKeyGetRequest::ofKey($shoppingList->getKey());
-        $response = $request->executeWithClient($this->getClient());
-        $result = $request->mapResponse($response);
+        ShoppingListFixture::withShoppingList(
+            $client,
+            function (ShoppingList $shoppingList) use ($client) {
+                $request = RequestBuilder::of()->shoppingLists()->getByKey($shoppingList->getKey());
+                $response = $this->execute($client, $request);
+                $result = $request->mapFromResponse($response);
 
-        $this->assertInstanceOf(ShoppingList::class, $shoppingList);
-        $this->assertSame($shoppingList->getId(), $result->getId());
+                $this->assertInstanceOf(ShoppingList::class, $shoppingList);
+                $this->assertSame($shoppingList->getId(), $result->getId());
+                $this->assertSame($shoppingList->getKey(), $result->getKey());
+            }
+        );
     }
 }
