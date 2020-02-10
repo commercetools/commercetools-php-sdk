cedb0b017a9daf655fdfba031174992558858650
Barbara Palumbo <barbara.palumbo@commercetools.de>
2020-02-10 15:45:01 +0100

    feat(CustomerStore): add set add remove action for store in customer and add tests

---
+++ src/Core/Request/Customers/Command/CustomerRemoveStoreAction.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Commercetools\Core\Request\Customers\Command;
+
+use Commercetools\Core\Model\Common\Context;
+use Commercetools\Core\Model\Store\StoreReference;
+use Commercetools\Core\Request\AbstractAction;
+
+/**
+ * @package Commercetools\Core\Request\Customers\Command
+ * @link https://docs.commercetools.com/http-api-projects-customers.html#remove-store-beta
+ * @method string getAction()
+ * @method CustomerRemoveStoreAction setAction(string $action = null)
+ * @method StoreReference getStore()
+ * @method CustomerRemoveStoreAction setStore(StoreReference $store = null)
+ */
+class CustomerRemoveStoreAction extends AbstractAction
+{
+    public function fieldDefinitions()
+    {
+        return [
+            'action' => [static::TYPE => 'string'],
+            'store' => [static::TYPE => StoreReference::class]
+        ];
+    }
+
+    /**
+     * @param array $data
+     * @param Context|callable $context
+     */
+    public function __construct(array $data = [], $context = null)
+    {
+        parent::__construct($data, $context);
+        $this->setAction('removeStore');
+    }
+}
