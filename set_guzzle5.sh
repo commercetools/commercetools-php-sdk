3d1eb97201382c644865da91a28bec3fbb7f8716
Jens Schulze <jenschude@gmail.com>
2020-10-12 13:38:41 +0200

    chore(Guzzle): add guzzle7 support

--- set_guzzle5.sh
+++ set_guzzle5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-SRC='"guzzlehttp/guzzle": "^6.0"'
+SRC='"guzzlehttp/guzzle": "^7.0 || ^6.0"'
 DST='"guzzlehttp/guzzle": "^5.3.3", "react/promise": "^2.2.0", "guzzlehttp/ringphp": "^1.1.1"'
 sed -ibak -e "s|$SRC|$DST|g" composer.json
