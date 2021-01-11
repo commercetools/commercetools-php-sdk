ff5c35168c7efd77a3b28010a485c3ccca126782
Barbara Palumbo <barbara.palumbo@commercetools.de>
2021-01-11 13:23:39 +0100

    docs(Client): update client version

--- src/Core/AbstractHttpClient.php
+++ src/Core/AbstractHttpClient.php
@@ -14,7 +14,7 @@ use Commercetools\Core\Client\Adapter\AdapterInterface;
  */
 abstract class AbstractHttpClient
 {
-    const VERSION = '2.13.0';
+    const VERSION = '2.13.0-dev';
 
     /**
      * @var AdapterInterface
