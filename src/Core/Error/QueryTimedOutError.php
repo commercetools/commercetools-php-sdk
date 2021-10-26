153cc0bdd3eee9d0adff330aeefb176f4a6c0e7d
Barbara Palumbo <barbara.palumbo@commercetools.de>
2021-10-26 17:56:02 +0200

    feat(Error): add querytimeout error

--- src/Core/Error/QueryTimedOutError.php
+++ src/Core/Error/QueryTimedOutError.php
@@ -3,7 +3,14 @@
 
 namespace Commercetools\Core\Error;
 
-
+/**
+ * @package Commercetools\Core\Error
+ *
+ * @method string getCode()
+ * @method QueryTimedOutError setCode(string $code = null)
+ * @method string getMessage()
+ * @method QueryTimedOutError setMessage(string $message = null)
+ */
 class QueryTimedOutError extends ApiError
 {
     const CODE = 'QueryTimedOut';
