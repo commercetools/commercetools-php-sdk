448a00997318a16da72d0789660ea7164623ed7f
Barbara Palumbo <barbara.palumbo@commercetools.de>
2022-01-31 14:49:27 +0100

    WIP: update tests with 404 code

--- tests/integration/Product/ProductHeadRequestTest.php
+++ tests/integration/Product/ProductHeadRequestTest.php
@@ -3,37 +3,21 @@
 namespace Commercetools\Core\IntegrationTests\Product;
 
 use Commercetools\Core\Builder\Request\RequestBuilder;
+use Commercetools\Core\Error\NotFoundException;
 use Commercetools\Core\IntegrationTests\ApiTestCase;
-use Commercetools\Core\Model\Common\Money;
-use Commercetools\Core\Model\Common\PriceDraft;
-use Commercetools\Core\Model\Common\PriceDraftCollection;
 use Commercetools\Core\Model\Product\Product;
-use Commercetools\Core\Model\Product\ProductVariantDraft;
+use Commercetools\Core\Model\Product\ProductDraft;
 
 class ProductHeadRequestTest extends ApiTestCase
 {
-    protected function getProductVariantDraft()
-    {
-        return ProductVariantDraft::ofSkuAndPrices(
-            'sku' . uniqid(),
-            PriceDraftCollection::of()->add(
-                PriceDraft::ofMoney(Money::ofCurrencyAndAmount('EUR', 100))
-            )
-        );
-    }
     public function testProductsExist()
     {
         $client = $this->getApiClient();
 
-        ProductFixture::withProduct(
-            $client,
-            function (Product $product) use ($client) {
-                $request = RequestBuilder::of()->products()->head();
-                $response = $this->execute($client, $request);
+        $request = RequestBuilder::of()->products()->head();
+        $response = $this->execute($client, $request);
 
-                $this->assertSame(200, $response->getStatusCode());
-            }
-        );
+        $this->assertSame(200, $response->getStatusCode());
     }
 
     public function testProductByIdExists()
@@ -51,19 +35,40 @@ class ProductHeadRequestTest extends ApiTestCase
         );
     }
 
+    public function testProductByIdNotExists()
+    {
+        $this->expectExceptionCode(404);
+        $client = $this->getApiClient();
+
+        $request = RequestBuilder::of()->products()->getByIdHead("test-id");
+        $this->execute($client, $request);
+    }
+
     public function testProductByKeyExists()
     {
         $client = $this->getApiClient();
 
-        ProductFixture::withProduct(
+        ProductFixture::withDraftProduct(
             $client,
+            function (ProductDraft $draft) {
+                return $draft->setKey(ProductFixture::uniqueProductString());
+            },
             function (Product $product) use ($client) {
-                $request = RequestBuilder::of()->products()->getByIdHead($product->getKey());
+                $request = RequestBuilder::of()->products()->getByKeyHead($product->getKey());
                 $response = $this->execute($client, $request);
 
                 $this->assertSame(200, $response->getStatusCode());
             }
         );
+    }
+
+    public function testProductByKeyNotExists()
+    {
+        $this->expectExceptionCode(404);
+        $client = $this->getApiClient();
+
+        $request = RequestBuilder::of()->products()->getByKeyHead("test-key");
+        $this->execute($client, $request);
     }
 
     public function testProductByQueryPredicateExists()
@@ -80,5 +85,15 @@ class ProductHeadRequestTest extends ApiTestCase
                 $this->assertSame(200, $response->getStatusCode());
             }
         );
+    }
+
+    public function testProductByQueryPredicateNotExists()
+    {
+        $this->expectExceptionCode(404);
+        $client = $this->getApiClient();
+
+        $request = RequestBuilder::of()->products()->head()
+            ->where('id=:id', ['id' => "88f6556a-239f-46c9-9fec-b9535f60272b"]);
+        $this->execute($client, $request);
     }
 }
