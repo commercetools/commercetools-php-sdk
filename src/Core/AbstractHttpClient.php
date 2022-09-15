5169005c2414567930ac87391a8180d9addad6b2
Barbara Palumbo <barbara.palumbo@commercetools.de>
2022-09-15 10:09:22 +0200

    docs(Changelog): update changelog

--- src/Core/AbstractHttpClient.php
+++ src/Core/AbstractHttpClient.php
@@ -14,7 +14,7 @@ use Commercetools\Core\Client\Adapter\AdapterInterface;
  */
 abstract class AbstractHttpClient
 {
-    const VERSION = '2.20.0';
+    const VERSION = '2.21.0';
 
     /**
      * @var AdapterInterface
